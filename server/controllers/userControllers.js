42f536e87b86922d2548f79e90b60285381ca9c2
Shakiba448 <shakiba448@gmail.com>
2021-05-20 09:41:07 +0600

    WIP on authentication

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -2,28 +2,6 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
-// @Description Auth data
-// @routes Post/api/users/login
-// @access Public
-
-export const authUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email });
-
-  if (user && (await user.matchPassword(password))) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
-  }
-});
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
@@ -54,5 +32,27 @@ export const registerUser = asyncHandler(async (req, res) => {
   } else {
     res.status(400);
     throw new Error("Invalid user data");
+  }
+});
+
+// @Description Auth data
+// @routes Post/api/users/login
+// @access Public
+
+export const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
   }
 });
