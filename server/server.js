f137a574e90109dfdc24b37c7df6b045c40b877e
Shakiba448 <shakiba448@gmail.com>
2021-05-20 09:40:02 +0600

    WIP on authentication

--- server/server.js
+++ server/server.js
@@ -4,7 +4,8 @@ import connectDB from "./config/db.js";
 import "colors";
 import morgan from "morgan";
 import cors from "cors";
-
+import userRoutes from "./routes/userRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
@@ -21,9 +22,17 @@ dotenv.config();
 
 connectDB();
 
+// Api routes
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is running....");
 });
+
+// Not found api
+app.use(notFound);
+// errorHandler
+app.use(errorHandler);
 
 app.listen(
   PORT,
