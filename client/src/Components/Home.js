7a1ab308ed29be5e22c750585a3d9cede147cee4
Shakiba448 <shakiba448@gmail.com>
2021-05-22 09:59:10 +0600

    WIP on section1

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,9 +4,13 @@ import {
   makeStyles,
   Typography,
   Paper,
+  TextField,
+  Button,
+  Box,
 } from "@material-ui/core";
 import clsx from "clsx";
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -23,11 +27,43 @@ const useStyles = makeStyles(() => ({
 }));
 const Home = () => {
   const classes = useStyles();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data, e) => {
+    console.log(data);
+    e.target.reset();
+  };
+
   return (
     <Container className={clsx(classes.root)}>
       <Grid item container justify="center" md={8} lg={8} sm={12}>
         <Paper className={clsx(classes.paper)}>
-          <Typography variant="h4">Welcome Home</Typography>
+          <Typography variant="h4">Welcome Khoj</Typography>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container direction="column">
+              <TextField
+                id="outlined-basic"
+                label="Search"
+                variant="outlined"
+                style={{ margin: "10px 0" }}
+                inputProps={{ ...register("email") }}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Result"
+                variant="outlined"
+                type="password"
+                style={{ margin: "10px 0" }}
+                inputProps={{ ...register("password") }}
+              />
+              <Button variant="contained" type="submit" color="primary">
+                Khoj
+              </Button>
+              <Box mt={1}>
+                <Typography variant="subtitle1">Result : </Typography>
+              </Box>
+            </Grid>
+          </form>
         </Paper>
       </Grid>
     </Container>
