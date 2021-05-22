52c692b8ef6a013b593c8843fcf4b299a34440fa
Shakiba448 <shakiba448@gmail.com>
2021-05-22 09:50:28 +0600

    WIP on section1

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,34 @@
-import { Container, Grid, Typography } from "@material-ui/core";
+import {
+  Container,
+  Grid,
+  makeStyles,
+  Typography,
+  Paper,
+} from "@material-ui/core";
+import clsx from "clsx";
 import React from "react";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    height: "100vh",
+    alignItems: "center",
+  },
+  paper: {
+    padding: "20px 40px",
+    boxShadow: "5px 5px 50px lightgray",
+    borderRadius: "15px",
+  },
+}));
 const Home = () => {
+  const classes = useStyles();
   return (
-    <Container>
-      <Grid container>
-        <Typography variant="h4">Welcome Home</Typography>
+    <Container className={clsx(classes.root)}>
+      <Grid item container justify="center" md={8} lg={8} sm={12}>
+        <Paper className={clsx(classes.paper)}>
+          <Typography variant="h4">Welcome Home</Typography>
+        </Paper>
       </Grid>
     </Container>
   );
