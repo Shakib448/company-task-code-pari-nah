374b3244818d0fb75c6fcaff8a5d6b18ca8a392d
Shakiba448 <shakiba448@gmail.com>
2021-05-22 10:24:56 +0600

    WIP on section1

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -10,7 +10,7 @@ import {
 } from "@material-ui/core";
 import clsx from "clsx";
 import React, { useState } from "react";
-// import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -27,45 +27,47 @@ const useStyles = makeStyles(() => ({
 }));
 const Home = () => {
   const classes = useStyles();
-  // const { register, handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
   const [search, setSearch] = useState([]);
   const [result, setResult] = useState([]);
-  console.log(Object.values(search));
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (data, e) => {
+    setSearch(data.search);
+    setResult(data.result);
   };
+
+  const isResult = Object.values(search).filter((value) =>
+    result.includes(value)
+  );
 
   return (
     <Container className={clsx(classes.root)}>
       <Grid item container justify="center" md={8} lg={8} sm={12}>
         <Paper className={clsx(classes.paper)}>
           <Typography variant="h4">Welcome Khoj</Typography>
-          <form onSubmit={onSubmit}>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Grid container direction="column">
               <TextField
                 id="outlined-basic"
                 label="Search"
                 variant="outlined"
                 style={{ margin: "10px 0" }}
-                // inputProps={{ ...register("search") }}
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
+                inputProps={{ ...register("search") }}
               />
               <TextField
                 id="outlined-basic"
                 label="Result"
                 variant="outlined"
                 style={{ margin: "10px 0" }}
-                // inputProps={{ ...register("result") }}
-                value={result}
-                onChange={(e) => setResult(e.target.value)}
+                inputProps={{ ...register("result") }}
               />
               <Button variant="contained" type="submit" color="primary">
                 Khoj
               </Button>
               <Box mt={1}>
-                <Typography variant="subtitle1">Result : </Typography>
+                <Typography variant="subtitle1">
+                  Result : {isResult.length !== 0 ? "True" : "False"}
+                </Typography>
               </Box>
             </Grid>
           </form>
